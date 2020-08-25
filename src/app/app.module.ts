ef941ce9e4b42d5da2e371a355ab68835efc63a0
Mhmod Gonim <69145711+gonimy@users.noreply.github.com>
2020-08-25 13:15:59 +0200

    validate the input form before submitting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { TodoTasksComponent } from './components/todo-tasks/todo-tasks.component
 import { HeaderComponent } from './components/header/header.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -23,6 +23,7 @@ import { FooterComponent } from './components/footer/footer.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule
 
   ],
   providers: [TodoServiceService],
--- src/app/components/todo-input/todo-input.component.ts
+++ src/app/components/todo-input/todo-input.component.ts
@@ -1,5 +1,6 @@
 import { TodoServiceService } from './../../services/todo-service.service';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-todo-input',
@@ -9,16 +10,37 @@ import { Component, OnInit } from '@angular/core';
 export class TodoInputComponent implements OnInit {
 
   public taskTitle: string
+  inputForm: FormGroup
 
   constructor(private todoeService: TodoServiceService) { }
 
   ngOnInit(): void {
+    this.inputForm = new FormGroup({
+      'taskTitle': new FormControl(null, [Validators.required, this.CheckEmpty])
+    })
   }
 
 
+
+
+
+
+
   addTask(): void {
-    this.todoeService.addTask(this.taskTitle);
-    this.taskTitle = ''
+    if (this.inputForm.valid) {
+
+      this.todoeService.addTask(this.taskTitle);
+      this.taskTitle = ''
+      this.inputForm.reset()
+    }
+  }
+
+
+  CheckEmpty(control: FormControl): { [s: string]: true } {
+    if (control.value === ' ') {
+      return { 'empty': true }
+    }
+    return null
   }
 
 
